a59b318b78ffc66066f305d7307ea60f8c56361e
Rafa Cruz <rafaelcruzroman@gmail.com>
2021-02-14 12:02:11 +0100

    Test async nodejs

--- node-basic/app.js
+++ node-basic/app.js
@@ -2,7 +2,10 @@ const helpers = require('./helpers');
 const http = require('http');
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const fileName = 'targetFile.txt';
 
+/*
 console.log("Test for nodejs");
 
 const totalSum = helpers.sum(10, 200);
@@ -10,13 +13,13 @@ const totalSub = helpers.sub(200, 10);
 console.log("TOTAL SUM: " + totalSum);
 console.log("TOTAL SUB: " + totalSub);
 
-/*
+
 const server = http.createServer ((req, res) => {
 	res.end("hello world for server request");
 })
-*/
 
-//server.listen(3000); 
+
+server.listen(3000); 
 
 app.get('/', (req, res) => {
 
@@ -24,3 +27,14 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000);
+
+*/
+
+fs.readFile(fileName, (err, data) => {
+	if(err) {
+		console.log(err);
+	}
+	console.log(data.toString());
+})
+
+console.log('test async nodejs');
